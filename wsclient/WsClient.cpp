b4f1f44f990a7e7db468b071f041933414d483ee
Zhu wei <zhuwei@quantdo.com.cn>
2021-12-17 17:30:29 +0800

    更新

--- wsclient/WsClient.cpp
+++ wsclient/WsClient.cpp
@@ -10,49 +10,79 @@
 
 #include <iostream>
 
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
+
 typedef websocketpp::client<websocketpp::config::asio_client> client;
 
-void on_message(websocketpp::connection_hdl, client::message_ptr msg) {
-	std::cout << msg->get_payload() << std::endl;
-}
-
-int main(int argc, char* argv[]) {
-    client c;
-
-    std::string uri = "ws://localhost:9002";
-
-    if (argc == 2) {
-        uri = argv[1];
-    }
-
-	try {
+class send_msg_to_broad
+{
+public:
+    send_msg_to_broad()
+    {
         // Set logging to be pretty verbose (everything except message payloads)
-        c.set_access_channels(websocketpp::log::alevel::all);
-        c.clear_access_channels(websocketpp::log::alevel::frame_payload);
-        c.set_error_channels(websocketpp::log::elevel::all);
+        m_client.set_access_channels(websocketpp::log::alevel::all);
+        m_client.clear_access_channels(websocketpp::log::alevel::frame_payload);
+        m_client.set_error_channels(websocketpp::log::elevel::all);
 
         // Initialize ASIO
-        c.init_asio();
+        m_client.init_asio();
 
-        // Register our message handler
-        c.set_message_handler(&on_message);
+        // Register our handlers
+        m_client.set_message_handler(bind(&send_msg_to_broad::on_message, this, ::_1, ::_2));
+        m_client.set_open_handler(bind(&send_msg_to_broad::on_open, this, ::_1));
+        m_client.set_close_handler(bind(&send_msg_to_broad::on_close, this, ::_1));
+    }
 
+    void start(std::string uri) {
         websocketpp::lib::error_code ec;
-        client::connection_ptr con = c.get_connection(uri, ec);
+        client::connection_ptr con = m_client.get_connection(uri, ec);
         if (ec) {
             std::cout << "could not create connection because: " << ec.message() << std::endl;
-            return 0;
+            return;
         }
 
         // Note that connect here only requests a connection. No network messages are
         // exchanged until the event loop starts running in the next line.
-        c.connect(con);
+        m_client.connect(con);
 
         // Start the ASIO io_service run loop
         // this will cause a single connection to be made to the server. c.run()
         // will exit when this connection is closed.
-        c.run();
+        m_client.run();
+    }
+
+    void on_open(websocketpp::connection_hdl hdl)
+    {
+        std::cout << "on_open" << std::endl;
+        m_client.send(hdl, "hello server1", websocketpp::frame::opcode::text);
+    }
+
+    void on_message(websocketpp::connection_hdl hdl, client::message_ptr msg)
+    {
+        std::cout << "on_message: " << msg->get_payload() << std::endl;
+    }
+
+    void on_close(websocketpp::connection_hdl hdl)
+    {
+        std::cout << "on_close" << std::endl;
+    }
+
+private:
+    client m_client;
+};
+
+int main(int argc, char* argv[]) {
+
+    std::string uri = "ws://localhost:9002";
+
+	try {
+        send_msg_to_broad client;
+
+        client.start(uri);
     } catch (websocketpp::exception const & e) {
         std::cout << e.what() << std::endl;
     }
 }
+
--- wsserver/WsServer.cpp
+++ wsserver/WsServer.cpp
@@ -1,31 +1,153 @@
-#ifndef ASIO_STANDALONE
+﻿#ifndef ASIO_STANDALONE
 #define ASIO_STANDALONE
 #define _WEBSOCKETPP_CPP11_RANDOM_DEVICE_
 #define _WEBSOCKETPP_CPP11_TYPE_TRAITS_
 #define _SCL_SECURE_NO_WARNINGS
 #endif // !ASIO_STANDALONE
 
+#include <chrono>
+#include <cstring>
+#include <ctime>
+#include <fstream>
+#include <future>
 #include <iostream>
+#include <mutex>
+#include <set>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <thread>
+#include <time.h>
+#include <vector>
 
 #include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/config/core.hpp>
 #include <websocketpp/server.hpp>
 
-typedef websocketpp::server<websocketpp::config::asio> server;
+using namespace std;
+// using ws = websocketpp;
 
-void on_message(websocketpp::connection_hdl, server::message_ptr msg) {
-    std::cout << msg->get_payload() << std::endl;
+using server = websocketpp::server<websocketpp::config::asio>;
+using Connections = std::set<websocketpp::connection_hdl, std::owner_less<websocketpp::connection_hdl>>;
+std::mutex conMutex;
+
+using websocketpp::connection_hdl;
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
+
+
+string Int_to_string(int n)
+{
+    ostringstream stream;
+    stream << n;
+    return stream.str();
 }
 
-int main() {
-    server print_server;
+class CWsServer
+{
+public:
+    CWsServer()
+    {
+        // Initialize Asio Transport
+        m_webSocketServer.init_asio();
 
-    print_server.set_message_handler(&on_message);
-    print_server.set_access_channels(websocketpp::log::alevel::all);
-    print_server.set_error_channels(websocketpp::log::elevel::all);
+        // Register handler callbacks
+        m_webSocketServer.set_open_handler(bind(&CWsServer::on_open, this, ::_1));
+        m_webSocketServer.set_close_handler(bind(&CWsServer::on_close, this, ::_1));
+        m_webSocketServer.set_message_handler(bind(&CWsServer::on_message, this, ::_1, ::_2));
+    }
 
-    print_server.init_asio();
-    print_server.listen(9002);
-    print_server.start_accept();
+    void run(uint16_t port)
+    {
+        // listen on specified port
+        m_webSocketServer.listen(port);
 
-    print_server.run();
+        // Start the server accept loop
+        m_webSocketServer.start_accept();
+
+        // Start the ASIO io_service run loop
+        try
+        {
+            m_webSocketServer.run();
+        }
+        catch (const std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+    }
+
+    void on_open(connection_hdl hdl)
+    {
+        cout << "on_open" << endl;
+        lock_guard<mutex> lock(conMutex);
+        m_connections.insert(hdl);
+    }
+
+    void on_close(connection_hdl hdl)
+    {
+        cout << "on_close" << endl;
+        lock_guard<mutex> lock(conMutex);
+        m_connections.erase(hdl);
+    }
+
+    void on_message(connection_hdl hdl, server::message_ptr msg)
+    {
+        cout << "on_message" << endl;
+        std::cout << "server received: " + msg->get_payload() << std::endl;
+    }
+
+    string generateMessage()
+    {
+        static vector<unsigned char> buffer;
+
+        int64_t timestamp = time(0);
+        string msg = "";
+        msg += R"({"timestamp":)" + Int_to_string(timestamp) +
+            R"(, "id":"5555", "payload":"hello world"})";
+
+        return msg;
+    }
+
+    void send_messages()
+    {
+        int nSendTimes = 1;
+        while (1)
+        {
+            string msg = generateMessage();
+            msg = msg + "send Times: " + Int_to_string(nSendTimes);
+
+            lock_guard<mutex> lock(conMutex);
+            for (auto &con : m_connections)
+            {
+                m_webSocketServer.send(
+                    con, msg, websocketpp::frame::opcode::text);
+            }
+            nSendTimes++;
+            Sleep(1000);
+        }
+    }
+
+private:
+    server m_webSocketServer;
+    Connections m_connections;
+};
+
+int main(int argc, char **argv)
+{
+    try
+    {
+        CWsServer server_instance;
+
+        thread t(bind(&CWsServer::send_messages, &server_instance));
+
+        // Run the asio loop with the main thread
+        server_instance.run(9002);
+        //启动发送消息的线程
+        t.join();
+    }
+    catch (websocketpp::exception const &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
 }
